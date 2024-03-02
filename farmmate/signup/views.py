97421207ec9a02e4c99d8041bbd123cf25c5ad56
mayur5204 <143496615+mayur5204@users.noreply.github.com>
2024-03-02 20:41:08 +0530

    chaged view.py

--- farmmate/signup/views.py
+++ farmmate/signup/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
 from django.contrib.auth.forms import UserCreationForm
 
 def signup(request):
-
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -12,12 +10,9 @@ def signup(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('login')
-        
+        else:
+            messages.error(request, 'There was an error with your submission')
     else:
         form = UserCreationForm()
 
-        return render(request, 'signup/signup.html', {'form': form})
-
-
-
-    
+    return render(request, 'signup/signup.html', {'form': form})
