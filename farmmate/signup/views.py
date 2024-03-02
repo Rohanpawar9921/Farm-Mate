a0f06ff12c17ab3615ac444291d17d0410ac9666
mayur5204 <143496615+mayur5204@users.noreply.github.com>
2024-03-02 20:13:45 +0530

    Project initiated with basic signup page

---
+++ farmmate/signup/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from django.contrib.auth.forms import UserCreationForm
+
+def signup(request):
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+        
+    else:
+        form = UserCreationForm()
+
+        return render(request, 'signup/signup.html', {'form': form})
+
+
+
+    
